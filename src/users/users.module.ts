9fe5529bd6669342a60bcea043b66b15f80df67f
illia tasminskyi <illia.tasminskyi@gmail.com>
2022-12-09 23:01:20 +0200

    users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users])],
   providers: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
